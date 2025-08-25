50f64ad7e6c94c0571c0411f0c887b05fa355f74
monga18 <163844830+monga18@users.noreply.github.com>
2025-08-26 01:30:12 +0900

    monga

--- frontend/booking-step3.js
+++ frontend/booking-step3.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
         image_url: "/images/map_muhuel.png", // 파일명/경로 실제와 꼭 일치 확인!
         sections: [
             // 아래 좌표들은 이미지의 중심을 기준으로 한 정확한 비율 값입니다.
-            { name: "제1곡 봉비암", top: "65%", left: "75%", width: "5%", height: "5%" },
-            { name: "제2곡 한강대", top: "47%", left: "63%", width: "5%", height: "5%" },
-            { name: "제3곡 무학정", top: "16%", left: "48%", width: "5%", height: "5%" },
-            { name: "제4곡 임압",   top: "87%", left: "71%", width: "5%", height: "5%" },
-            { name: "제5곡 사인암", top: "55%", left: "29%", width: "5%", height: "5%" },
-            { name: "제6곡 옥류동", top: "59%", left: "17%", width: "5%", height: "5%" }
+            { name: "제1곡 봉비암", top: "52%", left: "73%", width: "5%", height: "5%" },
+            { name: "제2곡 한강대", top: "46%", left: "70%", width: "5%", height: "5%" },
+            { name: "제3곡 무학정", top: "18%", left: "49%", width: "5%", height: "5%" },
+            { name: "제4곡 임압",   top: "78%", left: "83%", width: "5%", height: "5%" },
+            { name: "제5곡 사인암", top: "36%", left: "32%", width: "5%", height: "5%" },
+            { name: "제6곡 옥류동", top: "49%", left: "18%", width: "5%", height: "5%" }
         ]
     },
     "삼계리 계곡": {
